a586d2de6bef2641853f82ce117b725f31b8c01f
kelvinpraises <kelvinpraises@gmail.com>
2024-01-31 00:00:03 +0100

    feat: add all comps

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+import collider from "@/Library/collider";
+import AllBounties from "@/Library/components/organisms/AllBounties";
+import { useEffect, useRef } from "react";
+
 const page = () => {
-  return <div>home</div>;
+  const canvasParentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    collider({ canvasParentRef });
+  }, []);
+
+  return (
+    <div className="w-full">
+      <div
+        className="w-full min-h-[5rem] flex flex-1 items-center justify-center"
+        ref={canvasParentRef}
+      ></div>
+      <AllBounties />
+    </div>
+  );
 };
 
 export default page;

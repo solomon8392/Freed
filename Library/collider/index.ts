746118bc3f39f331a52049f5923cb57c53f6e9b9
kelvinpraises <kelvinpraises@gmail.com>
2024-02-06 10:58:34 +0100

    feat: add Protest Riot google font

--- Library/collider/index.ts
+++ Library/collider/index.ts
@@ -24,10 +24,13 @@ const collider = ({
     velocity: new Vector(0, 0),
     fixed: true,
     text: "Explore Bounties!",
-    font: "200px bebas",
+    font: "250px 'Protest Riot'",
   });
 
   floaters.push(box);
+
+  // Randomly choose the starting index
+  const startIndex = Math.floor(Math.random() * itemList.length);
 
   const loadImages = async () => {
     const loadImage = (src: string): Promise<HTMLImageElement> => {
@@ -44,110 +47,115 @@ const collider = ({
     return Promise.all(imagePromises);
   };
 
-  // Randomly choose the starting index
-  const startIndex = Math.floor(Math.random() * itemList.length);
+  let loadFont = new FontFace(
+    "Protest Riot",
+    "url(https://fonts.gstatic.com/s/protestriot/v2/d6lPkaOxWMKm7TdezXFmpkr80PhhrGd5OQ.woff2)"
+  );
 
-  loadImages().then((images) => {
-    while (createdCount < count) {
-      let positionIsValid = true;
+  loadFont.load().then((font) => {
+    document.fonts.add(font);
+    loadImages().then((images) => {
+      while (createdCount < count) {
+        let positionIsValid = true;
 
-      const selectedItemIndex = (startIndex + createdCount) % itemList.length;
-      const svgImage = images[selectedItemIndex];
+        const selectedItemIndex = (startIndex + createdCount) % itemList.length;
+        const svgImage = images[selectedItemIndex];
 
-      const radius = Math.min(svgImage.width / 2.2, svgImage.height / 2.2)
+        const radius = Math.min(svgImage.width / 2.2, svgImage.height / 2.2);
 
-      const newBall = new Ball({
-        radius,
-        color: colors[random(colors.length - 1)],
-        position: new Vector(
-          random(width * 4 - (10 + radius), (10 + radius)),
-          random(height * 4 - (10 + radius), (10 + radius))
-        ),
-        velocity: new Vector(random(0.2, -0.2), random(0.2, -0.2)),
-        image: {
-          src: svgImage,
-          width: svgImage.width / 1.2,
-          height: svgImage.height / 1.2,
-        },
-      });
+        const newBall = new Ball({
+          radius,
+          color: colors[random(colors.length - 1)],
+          position: new Vector(
+            random(width * 4 - (10 + radius), 10 + radius),
+            random(height * 4 - (10 + radius), 10 + radius)
+          ),
+          velocity: new Vector(random(0.2, -0.2), random(0.2, -0.2)),
+          image: {
+            src: svgImage,
+            width: svgImage.width / 1.2,
+            height: svgImage.height / 1.2,
+          },
+        });
 
-      for (const otherFloater of floaters) {
-        const otherFloaterRadius =
-          "radius" in otherFloater ? otherFloater.radius : 0; // Check for radius if it's a circle
-        const otherFloaterDiagonal =
-          "width" in otherFloater && "height" in otherFloater
-            ? Math.sqrt(
-                Math.pow(otherFloater.width, 2) +
-                  Math.pow(otherFloater.height, 2)
-              )
-            : 0; // Check for diagonal if it's a square
+        for (const otherFloater of floaters) {
+          const otherFloaterRadius =
+            "radius" in otherFloater ? otherFloater.radius : 0; // Check for radius if it's a circle
+          const otherFloaterDiagonal =
+            "width" in otherFloater && "height" in otherFloater
+              ? Math.sqrt(
+                  Math.pow(otherFloater.width, 2) +
+                    Math.pow(otherFloater.height, 2)
+                )
+              : 0; // Check for diagonal if it's a square
 
-        if (
-          newBall.position.add(newBall.velocity).subtract(
-            otherFloater.type === "square" && otherFloater.fixed
-              ? new Vector((width * 4) / 2, (height * 4) / 2) // simulate the square origin point being at the center of canvas
-              : otherFloater.position.add(otherFloater.velocity)
-          ).magnitude <=
-          newBall.radius +
-            Math.max(otherFloaterRadius, otherFloaterDiagonal / 2)
-        ) {
-          positionIsValid = false;
+          if (
+            newBall.position.add(newBall.velocity).subtract(
+              otherFloater.type === "square" && otherFloater.fixed
+                ? new Vector((width * 4) / 2, (height * 4) / 2) // simulate the square origin point being at the center of canvas
+                : otherFloater.position.add(otherFloater.velocity)
+            ).magnitude <=
+            newBall.radius +
+              Math.max(otherFloaterRadius, otherFloaterDiagonal / 2)
+          ) {
+            positionIsValid = false;
+          }
+        }
+
+        if (positionIsValid) {
+          floaters.push(newBall);
+          createdCount++;
         }
       }
 
-      if (positionIsValid) {
-        floaters.push(newBall);
-        createdCount++;
-      }
-    }
+      while (smallerCreatedCount < 15) {
+        const newBall = new Ball({
+          radius: random(10, 15) + Math.random(),
+          color: "black",
+          position: new Vector(
+            random(width * 4 - 50, 50),
+            random(height * 4 - 50, 50)
+          ),
+          velocity: new Vector(random(1, -1), random(1, -1)),
+        });
 
-    while (smallerCreatedCount < 15) {
-      const newBall = new Ball({
-        radius: random(10, 15) + Math.random(),
-        color: "black",
-        position: new Vector(
-          random(width * 4 - 50, 50),
-          random(height * 4 - 50, 50)
-        ),
-        velocity: new Vector(random(1, -1), random(1, -1)),
-      });
+        let positionIsValid = true;
+        for (const otherFloater of floaters) {
+          const otherFloaterRadius =
+            "radius" in otherFloater ? otherFloater.radius : 0; // Check for radius if it's a circle
+          const otherFloaterDiagonal =
+            "width" in otherFloater && "height" in otherFloater
+              ? Math.sqrt(
+                  Math.pow(otherFloater.width, 2) +
+                    Math.pow(otherFloater.height, 2)
+                )
+              : 0; // Check for diagonal if it's a square
 
-      let positionIsValid = true;
-      for (const otherFloater of floaters) {
-        const otherFloaterRadius =
-          "radius" in otherFloater ? otherFloater.radius : 0; // Check for radius if it's a circle
-        const otherFloaterDiagonal =
-          "width" in otherFloater && "height" in otherFloater
-            ? Math.sqrt(
-                Math.pow(otherFloater.width, 2) +
-                  Math.pow(otherFloater.height, 2)
-              )
-            : 0; // Check for diagonal if it's a square
+          if (
+            newBall.position.add(newBall.velocity).subtract(
+              otherFloater.type === "square" && otherFloater.fixed
+                ? new Vector((width * 4) / 2, (height * 4) / 2) // simulate the square origin point being at the center of canvas
+                : otherFloater.position.add(otherFloater.velocity)
+            ).magnitude <
+            newBall.radius +
+              Math.max(otherFloaterRadius + 100, otherFloaterDiagonal / 2)
+          ) {
+            positionIsValid = false;
+            break;
+          }
+        }
 
-        if (
-          newBall.position.add(newBall.velocity).subtract(
-            otherFloater.type === "square" && otherFloater.fixed
-              ? new Vector((width * 4) / 2, (height * 4) / 2) // simulate the square origin point being at the center of canvas
-              : otherFloater.position.add(otherFloater.velocity)
-          ).magnitude <
-          newBall.radius +
-            Math.max(otherFloaterRadius + 100, otherFloaterDiagonal / 2)
-        ) {
-          positionIsValid = false;
-          break;
+        if (positionIsValid) {
+          floaters.push(newBall);
+          smallerCreatedCount++;
         }
       }
 
-      if (positionIsValid) {
-        floaters.push(newBall);
-        smallerCreatedCount++;
-      }
-    }
-
-    let state = new State(display, floaters);
-    runAnimation((time) => {
-      state = state.update(time);
-      display.sync(state);
+      let state = new State(display, floaters);
+      runAnimation((time) => {
+        state = state.update(time);
+        display.sync(state);
+      });
     });
   });
 };

b7ffc2e52cb8cd04a9e4cf4a599af999ca115ad6
kelvinpraises <kelvinpraises@gmail.com>
2024-02-06 07:49:47 +0100

    feat: implement home page

---
+++ app/(Home)/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import collider from "@/library/collider";
+import AllBounties from "@/library/components/organisms/AllBounties";
+import { useEffect, useRef } from "react";
+
+const page = () => {
+  const canvasParentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    collider({ canvasParentRef });
+  }, []);
+
+  return (
+    <div className="w-full flex flex-col gap-8">
+      <div
+        className="w-full min-h-[5rem] flex flex-1 items-center justify-center"
+        ref={canvasParentRef}
+      ></div>
+      <AllBounties />
+    </div>
+  );
+};
+
+export default page;
